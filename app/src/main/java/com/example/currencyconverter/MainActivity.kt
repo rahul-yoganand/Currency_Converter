9af6fff203a34a2f463508d62330559d8143ce89
rahulyoganand <pry10@iitbbs.ac.in>
2021-09-03 00:16:04 +0530

    viewmodel with livedata for currency conversion with api request(api not working)

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnConvert = findViewById(R.id.button)
         btnChange = findViewById(R.id.button2)
         tvResult = findViewById(R.id.textView)
         etDollars = findViewById(R.id.inputValue)
@@ -30,35 +29,28 @@ class MainActivity : AppCompatActivity() {
             ViewModelProvider(this).get(
                 CurrencyViewModel::class.java
             )
-        etDollars.addTextChangedListener {object :TextWatcher{
+        etDollars.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                mViewModel.getFactor()
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
+                mViewModel.getFactor()
+                    .observe(this@MainActivity, Observer<Double> { conversionfactor ->
+                        tvResult.text =
+                            (conversionfactor * (etDollars.text.toString().toDouble())).toString()
+                    })
             }
 
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
         }
 
-        }
-
-        tvResult.text =
-            (mViewModel.getFactor().value!! * etDollars.text.toString().toDouble()).toString()
+        )
 
         btnChange.setOnClickListener {
             mViewModel.setValue()
-        }
-        btnConvert.setOnClickListener {
-            mViewModel.getFactor().observe(this, Observer<Double> { conversionfactor ->
-                tvResult.text =
-                    (conversionfactor * (etDollars.text.toString().toDouble())).toString()
-            })
-
         }
 
     }
