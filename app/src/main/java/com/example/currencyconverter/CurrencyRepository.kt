35ccc391127ff1b86ba69c2e4bab5dd2e8592cd4
rahulyoganand <pry10@iitbbs.ac.in>
2021-09-02 23:51:21 +0530

    viewmodel with livedata for currency conversion with api request(not working)

--- app/src/main/java/com/example/currencyconverter/CurrencyRepository.kt
+++ app/src/main/java/com/example/currencyconverter/CurrencyRepository.kt
@@ -1,20 +1,45 @@
 package com.example.currencyconverter
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.net.URL
 
 
 class CurrencyRepository {
     private val mConversionFactor=MutableLiveData<Double>(70.00)
     fun getFactor(): MutableLiveData<Double> {
+        dataFromApi()
+        //change the mConversionfactor else it will return the old value
+
         return mConversionFactor
     }
     fun setValue() {
-        if(mConversionFactor.value==70.00)
         mConversionFactor.value=100.00
-        else
-            mConversionFactor.value=70.00
     }
     fun dataFromApi(){
+
         //we change the value of the factor and automatically the view gets updated
+        var API =
+            "https://api.exchangeratesapi.io/v1/latest?access_key=f26e9cf950b012caed9dc9c5f5d438c9&base=EUR&symbols=INR"
+        GlobalScope.launch(Dispatchers.IO) {
+
+
+            try {
+
+                val apiResult = URL(API).readText()
+                val jsonObject = JSONObject(apiResult)
+
+                mConversionFactor.value =
+                    jsonObject.getJSONObject("rates").getString("INR")
+                        .toDouble()
+                Log.d(CurrencyRepository::class.java.simpleName,"${mConversionFactor.value}")
+            } catch (e: Exception) {
+                Log.d(CurrencyRepository::class.java.simpleName,"Hello "+e)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.currencyconverter
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -27,14 +30,33 @@ class MainActivity : AppCompatActivity() {
             ViewModelProvider(this).get(
                 CurrencyViewModel::class.java
             )
-            tvResult.text = (mViewModel.getFactor().value!! * etDollars.text.toString().toDouble()).toString()
+        etDollars.addTextChangedListener {object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                mViewModel.getFactor()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        }
+
+        tvResult.text =
+            (mViewModel.getFactor().value!! * etDollars.text.toString().toDouble()).toString()
 
         btnChange.setOnClickListener {
             mViewModel.setValue()
         }
         btnConvert.setOnClickListener {
-            mViewModel.getFactor().observe(this,Observer<Double> { conversionfactor ->
-                tvResult.text=(conversionfactor*(etDollars.text.toString().toDouble())).toString()
+            mViewModel.getFactor().observe(this, Observer<Double> { conversionfactor ->
+                tvResult.text =
+                    (conversionfactor * (etDollars.text.toString().toDouble())).toString()
             })
 
         }
